7af48f40da9b5fbc32362d580f74e0da269bd714
Giovanni Zucolin <giovannizucolinluiz@gmail.com>
2022-08-28 15:59:26 -0300

    Criado forms

--- resources/views/categoria.blade.php
+++ resources/views/categoria.blade.php
@@ -1,8 +1,19 @@
 @extends('templates.default')
 @section('content')
 
-    <section>
+    <section class="categoria">
         <h1> Categoria </h1>
+
+        <form action="" method="post" class="form-produtos">
+
+            <div class="div-inputs">
+			    <input type="text" placeholder="Categoria" name="txCategoria">
+		    </div>
+		    <div class="div-inputs">
+			    <input type="submit" value="Salvar" class="btn-salvar">
+		    </div>
+
+	    </form>
     </section>
 
 @endsection
--- resources/views/pedido.blade.php
+++ resources/views/pedido.blade.php
@@ -1,8 +1,25 @@
 @extends('templates.default')
 @section('content')
 
-    <section>
+    <section class="pedido">
         <h1> Pedido </h1>
+
+        <form action="" method="post" class="form-produtos">
+
+            <div class="div-inputs">
+			    <input type="text" placeholder="Produto" name="txProduto">
+		    </div>
+            <div class="div-inputs">
+			    <input type="text" placeholder="Cliente" name="txCliente">
+		    </div>
+            <div class="div-inputs">
+			    <input type="text" placeholder="Data" name="txData">
+		    </div>
+		    <div class="div-inputs">
+			    <input type="submit" value="Salvar" class="btn-salvar">
+		    </div>
+
+	    </form>
     </section>
 
 @endsection
